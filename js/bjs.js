26eb446ebc2577117ad7fcef3f45c02359189a2e
nauval <sigantengnoval@gmail.com>
2020-03-02 15:14:15 +0700

    javascript

---
+++ js/bjs.js
@@ -0,0 +1,24 @@
+function openlist1() {
+    var x = document.getElementById("list1");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function openlist2() {
+    var x = document.getElementById("list2");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function openlist3() {
+    var x = document.getElementById("list3");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
